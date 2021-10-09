366183482fd179b03002c96da48f7284b0f3fc3d
acostaj19 <91224459+acostaj19@users.noreply.github.com>
2021-10-09 18:09:15 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, redirect, render_template, request, url_for, session, flash
 from functools import wraps
-from forms import FormIndex
 import os
+from forms import Cambio_pass, Registro, Retroalimentacion, Info_personal
 
 
 # create the application object
@@ -26,7 +26,6 @@ def login_required(f):
         if 'logged_in' in session:
             return f(*args, **kwargs)
         else:
-            flash('Para continuar es necesario que inicie sesión.')
             return redirect(url_for('login'))
     return wrap
 
@@ -35,13 +34,36 @@ def login_required(f):
 @app.route('/')
 @login_required
 def home():
-    form = FormIndex()
     # return "Hello, World!"  # return a string
-    return render_template('index.html', form=form)  # render a template
+    return render_template('index.html')  # render a template
 
-@app.route('/welcome')
-def welcome():
-    return render_template('welcome.html')  # render a template
+@app.route('/registro', methods=['GET', 'POST'])
+@login_required
+def registro():
+    # return "Hello, World!"  # return a string
+    form = Registro()
+    return render_template('registro.html', form=form)  # render a template
+
+@app.route('/retroal', methods=['GET', 'POST'])
+@login_required
+def retroal():
+    # return "Hello, World!"  # return a string
+    form = Retroalimentacion()
+    return render_template('retroal.html', form=form)  # render a template
+
+@app.route('/cambiopass', methods=['GET', 'POST'])
+@login_required
+def cambiopass():
+    # return "Hello, World!"  # return a string
+    form = Cambio_pass()
+    return render_template('cambiopass.html', form=form)  # render a template
+
+@app.route('/infoper', methods=['GET', 'POST'])
+@login_required
+def infoper():
+    # return "Hello, World!"  # return a string
+    form = Info_personal()
+    return render_template('infoper.html', form=form)  # render a template
 
 # Route for handling the login page logic
 @app.route('/login', methods=['GET', 'POST'])
@@ -52,12 +74,12 @@ def login():
         username = request.form['username']
         password = request.form['password']
 
+        home(username)
         # Change username and password to test
         if username != 'admin' or password != 'admin':
             error = 'Credenciales invalidas. Por favor intente de nuevo.'
         else:
             session['logged_in'] = True
-            flash('Usuario Logueado!')
             return redirect(url_for('home'))
     return render_template('login.html', error=error)
 
@@ -66,7 +88,8 @@ def login():
 def logout():
     session.pop('logged_in', None)
     flash('Usuario Deslogueado!')
-    return redirect(url_for('welcome'))
+    return redirect(url_for('login'))
+
 
 # start the server with the 'run()' method
 if __name__ == '__main__':
--- forms.py
+++ forms.py
@@ -1,18 +1,45 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField
+from wtforms import StringField, SubmitField, SelectField, TextAreaField
 from wtforms.fields.html5 import DateField
+from wtforms.fields.simple import PasswordField
 from wtforms.validators import DataRequired
+from wtforms.widgets.core import TextArea
 
-class FormIndex(FlaskForm):
+class Registro(FlaskForm):
     pnombre = StringField('Primer Nombre', validators=[DataRequired(message='No dejar vacío, completar')])
     snombre = StringField('Segundo Nombre')
     papellido = StringField('Primer Apellido', validators=[DataRequired(message='No dejar vacío, completar')])
     sapellido = StringField('Segundo Apellido')
-    tipoid = SelectField('Tipo de Identificación', choices=[('0', '--Seleccione el Tipo de Id--'), ('1', 'Cédula de Ciudadanía'), ('2', 'Cédula de Extranjería'), ('3', 'Carné Diplomatico'), ('4', 'Pasaporte'), ('5', 'Tarjeta de Identidad'), ('6', 'Número Único de Identificación'), ('7', 'Permiso Especial de Permanencia'), ('8', 'Salvoconducto de Permanencia')], validators=[DataRequired(message='No dejar vacío, completar')])
+    tipoid = SelectField('Tipo de Identificación', choices=[('0', '--Tipo Id--'), ('1', 'Cédula de Ciudadanía'), ('2', 'Cédula de Extranjería'), ('3', 'Carné Diplomatico'), ('4', 'Pasaporte'), ('5', 'Tarjeta de Identidad')], validators=[DataRequired(message='No dejar vacío, completar')])
     idNum = StringField('Número de Identificación', validators=[DataRequired(message='No dejar vacío, completar')])
     fnacimiento = DateField('Fecha de Nacimiento', validators=[DataRequired(message='No dejar vacío, completar')], format='%Y-%m-%d')
     email = StringField('E-mail')
     direccion = StringField('Dirección')
     telefono = StringField('Teléfono')
     enviar = SubmitField('Registrar')
+
+class Retroalimentacion(FlaskForm):
+    retroalimtext=TextAreaField("Retroalimentacion")
+    puntajetext=StringField("Puntaje")
+
+class Cambio_pass(FlaskForm):
+    actualpass=PasswordField("Contraseña actual")
+    nuevopass=PasswordField("Nueva contraseña")
+    val_nuevopass=PasswordField("Confirme nueva contraseña")
+
+class Info_personal(FlaskForm):
+    pnombre = StringField('Primer Nombre')
+    snombre = StringField('Segundo Nombre')
+    papellido = StringField('Primer Apellido')
+    sapellido = StringField('Segundo Apellido')
+    tipoid = SelectField('Tipo de Identificación', choices=[('0', '--Tipo Id--'), ('1', 'Cédula de Ciudadanía'), ('2', 'Cédula de Extranjería'), ('3', 'Carné Diplomatico'), ('4', 'Pasaporte'), ('5', 'Tarjeta de Identidad')])
+    idNum = StringField('Número de Identificación')
+    fnacimiento = DateField('Fecha de Nacimiento')
+    email = StringField('E-mail')
+    direccion = StringField('Dirección')
+    telefono = StringField('Teléfono')
+    actualizar = SubmitField('Actualizar')
+    eliminar = SubmitField('Eliminar Registro')
+
+
 
