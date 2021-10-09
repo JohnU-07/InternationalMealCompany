10bb25d41de5bd1a5df6780b6c02fe647117e613
JohnU-07 <johnu@uninorte.edu.co>
2021-10-08 20:00:00 -0500

    [ADD] Styles to form in index.html

--- forms.py
+++ forms.py
@@ -8,7 +8,7 @@ class FormIndex(FlaskForm):
     snombre = StringField('Segundo Nombre')
     papellido = StringField('Primer Apellido', validators=[DataRequired(message='No dejar vacío, completar')])
     sapellido = StringField('Segundo Apellido')
-    tipoid = SelectField('Tipo de Identificación', choices=[('0', '--Tipo de Identificación--'), ('1', 'Cédula de Ciudadanía'), ('2', 'Cédula de Extranjería'), ('3', 'Carné Diplomatico'), ('4', 'Pasaporte'), ('5', 'Tarjeta de Identidad'), ('6', 'Número Único de Identificación'), ('7', 'Permiso Especial de Permanencia'), ('8', 'Salvoconducto de Permanencia')], validators=[DataRequired(message='No dejar vacío, completar')])
+    tipoid = SelectField('Tipo de Identificación', choices=[('0', '--Seleccione el Tipo de Id--'), ('1', 'Cédula de Ciudadanía'), ('2', 'Cédula de Extranjería'), ('3', 'Carné Diplomatico'), ('4', 'Pasaporte'), ('5', 'Tarjeta de Identidad'), ('6', 'Número Único de Identificación'), ('7', 'Permiso Especial de Permanencia'), ('8', 'Salvoconducto de Permanencia')], validators=[DataRequired(message='No dejar vacío, completar')])
     idNum = StringField('Número de Identificación', validators=[DataRequired(message='No dejar vacío, completar')])
     fnacimiento = DateField('Fecha de Nacimiento', validators=[DataRequired(message='No dejar vacío, completar')], format='%Y-%m-%d')
     email = StringField('E-mail')
